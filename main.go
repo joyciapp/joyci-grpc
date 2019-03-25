853d77d29a414d4f3c1173a48372c51314351273
Tony Oreste <tony.oreste@de.atu.eu>
2019-03-25 18:52:59 +0100

    [-] removes test code from main

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"time"
-
 	"github.com/joyciapp/joyci-grpc/grpc/api"
 )
 
 func main() {
-	go api.Serve()
-	api.GitClone("git@github.com:joyciapp/joyci-grpc.git")
-	time.Sleep(30 * time.Second)
+	// start server
+	api.Serve()
 }
